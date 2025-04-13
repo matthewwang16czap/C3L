7ccd54b2993c2662e1f055f52399aac8de471191
Sihan Wang <sw5796@nyu.edu>
2025-04-14 02:26:13 +0800

    add finetune.sh

---
+++ finetune.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+/home/matthew/miniconda3/envs/rocm/bin/deepspeed ./llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path /home/matthew/models/llava-v1.5-7b \
+    --version v1 \
+    --data_path ./C3L/high_I2C_question.json \
+    --image_folder /home/matthew/fiftyone/coco-2014/train/data \
+    --vision_tower /home/matthew/models/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter /home/matthew/models/llava-v1.5-7b/mm_projector.bin \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir ./checkpoints/llava-v1.5-7b-finetuned \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 False \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
